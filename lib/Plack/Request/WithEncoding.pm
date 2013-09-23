cdc4ea134b5b7bb8aefed2d626e42d9d66c1f239
moznion <moznion@gmail.com>
2013-09-23 21:54:03 +0900

    Implement `param()`.

--- lib/Plack/Request/WithEncoding.pm
+++ lib/Plack/Request/WithEncoding.pm
@@ -36,6 +36,14 @@ sub parameters {
 }
 
 sub param {
+    my $self = shift;
+
+    my $parameters = $self->parameters;
+    return keys %{ $parameters } if @_ == 0;
+
+    my $key = shift;
+    return $parameters->{$key} unless wantarray;
+    return $parameters->get_all($key);
 }
 
 sub raw_body_parameters {

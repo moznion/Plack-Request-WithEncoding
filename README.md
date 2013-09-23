844a14180a26936458cdc4dfe1a55c85585b4f06
moznion <moznion@gmail.com>
2013-09-23 16:37:54 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Plack::Request::WithEncoding - It's new $module
+
+# SYNOPSIS
+
+    use Plack::Request::WithEncoding;
+
+# DESCRIPTION
+
+Plack::Request::WithEncoding is ...
+
+# LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+moznion <moznion@gmail.com>
---
+++ lib/Plack/Request/WithEncoding.pm
@@ -0,0 +1,39 @@
+package Plack::Request::WithEncoding;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Plack::Request::WithEncoding - It's new $module
+
+=head1 SYNOPSIS
+
+    use Plack::Request::WithEncoding;
+
+=head1 DESCRIPTION
+
+Plack::Request::WithEncoding is ...
+
+=head1 LICENSE
+
+Copyright (C) moznion.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+moznion E<lt>moznion@gmail.comE<gt>
+
+=cut
+
